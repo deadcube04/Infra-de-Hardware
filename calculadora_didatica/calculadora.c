d66097a96497e1b21b0bd1418e8e5c18bd0b4765
Gabriel Albuquerque <deadcube004@gmail.com>
2024-08-27 11:49:19 -0300

    Questão 2

--- calculadora_didatica/calculadora.c
+++ calculadora_didatica/calculadora.c
@@ -2,54 +2,86 @@
  
  * created on Aug, 24th 2024
  * Author: Gabriel Albuquerque - 3ºB
- * Questâo 1 - 27/08/2024 11:10
+ * Questão 2 - 27/08/2024 11:49
  
  **/
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 
-int * conversor(int number, int base){
-    // int size = (int) log10(number) + 1;
-    int newNumber[100] = {0};
-    int j = 0;
-    int pNum = number;
-    char hexaArray[] = {'A', 'B', 'C', 'D', 'E', 'F'};
-    printf("dividindo %d por %d\n\n\n", number, base);
-    sleep(1);
-    for(number; number > 0; number/=base){
-        if (number%base >= 10){
-            newNumber[j] = hexaArray[(number%base)-10];
-            printf("modulo de %d por %d = %c\n\n", number, base , hexaArray[(number%base)-10]);
+    int * conversor(int number, int base){
+        // int size = (int) log10(number) + 1;
+        int newNumber[100] = {0};
+        int j = 0;
+        int pNum = number;
+        char hexaArray[] = {'A', 'B', 'C', 'D', 'E', 'F'};
+        printf("dividindo %d por %d\n\n\n", number, base);
+        sleep(1);
+        for(number; number > 0; number/=base){
+            if (number%base >= 10){
+                newNumber[j] = hexaArray[(number%base)-10];
+                printf("modulo de %d por %d = %c\n\n", number, base , hexaArray[(number%base)-10]);
 
+            }
+            else{
+                newNumber[j] = number%base;
+                printf("modulo de %d por %d = %d\n\n", number, base , number%base);
+
+            }
+            
+            sleep(1);
+        j++;
+        }
+
+        j-=1;
+        int k = 0;
+        int *revertArray = malloc(j * sizeof(int));
+        printf("invertendo restos da divisao...\n\n");
+        sleep(1);
+        printf(" %d na base %d = ", pNum, base);
+        if (base == 16)
+        {
+            for(j; j >= 0; j--){
+                revertArray[k] = newNumber[j];
+                if (revertArray[k] == 'A' || revertArray[k] == 'B' ||revertArray[k] == 'C' ||revertArray[k] == 'D' ||revertArray[k] == 'E' ||revertArray[k] == 'F')
+                {
+                    printf("%c", revertArray[k]);
+                }
+                else{
+
+                printf("%d", revertArray[k]);
+                }
+                
+                
+                k++;
+            }
         }
         else{
-            newNumber[j] = number%base;
-            printf("modulo de %d por %d = %d\n\n", number, base , number%base);
-
+            for(j; j >= 0; j--){
+                revertArray[k] = newNumber[j];
+                printf("%d", revertArray[k]);
+                k++;
+            }
         }
         
-        sleep(1);
-       j++;
+        
+        return revertArray;
     }
 
-    j-=1;
-    int k = 0;
-    int *revertArray = malloc(j * sizeof(int));
-    printf("invertendo restos da divisao...\n\n");
+
+void printBinary(char value, int n) {
+    printf("invertendo os digitos apos o primeiro 1...\n");
     sleep(1);
-    printf(" %d na base %d = ", pNum, base);
-    
-    for(j; j >= 0; j--){
-        revertArray[k] = newNumber[j];
-        printf("%c", revertArray[k]);
-        k++;
+    for (int i = n; i >= 0; i--) {
+        printf("%d", (value >> i) & 1);
+        sleep(1);
+
     }
-    
-    
-    return revertArray;
+
 }
 
 
@@ -73,6 +105,13 @@ char * toBCD(int number){
     return temp2;
 }
 
+unsigned int toComplementoA2(int num) {
+    printf("colocando numero para 16 bits...\n");
+    sleep(1);
+
+    return (unsigned int)(num & 0xFFFF);
+}
+
 int main(){
     printf("=========================================================================================================================\n\n\n");
     printf("Ola, bem-vindo(a) a calculadora conversora didatica\n\n");
@@ -83,7 +122,7 @@ int main(){
 
     scanf("%d", &number);
 
-    printf("\nPara qual base voce deseja converter\n [1] para decimal\n [2] para octal\n [3] para hexadecimal\n [4] para BCD\n\n\n");
+    printf("\nPara qual base voce deseja converter\n [1] para decimal\n [2] para octal\n [3] para hexadecimal\n [4] para BCD\n [5] para complemento A2 \n\n");
 
     scanf("%d", &choice);
     int base, bcd = 0;
@@ -110,6 +149,12 @@ int main(){
             printf("inicuiando conversao para BCD...\n");
 
             break;
+        case 5:
+            a2 = 1;
+            printf("inicuiando conversao para complemento a2...\n");
+            sleep(1);
+            break;
+
 
 
     }   
@@ -144,6 +189,12 @@ int main(){
         }
             printf("\n");
         free(bcdArray);
+    }
+        
+    else if (a2 == 1){
+
+        unsigned int result = toComplementoA2(number);
+        printBinary(result, 15);
     }
 
 
